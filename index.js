36dd8522d5b3f8aafde5917e7bac2d3e0e88c912
shariarhossain23e <shariarhossain23@gamil.comm>
2022-05-19 01:39:39 +0600

    post api

--- index.js
+++ index.js
@@ -1,39 +1,48 @@
-const express = require('express')
-const cors = require("cors")
-const app = express()
-require("dotenv").config()
-const port =process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require("express");
+const cors = require("cors");
+const app = express();
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
-// middleware 
-app.use(cors())
-app.use(express.json())
-
+// middleware
+app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.foxet.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
-async function run(){
-  try{
+async function run() {
+  try {
     await client.connect();
 
     const todoBooking = client.db("to_do").collection("data");
 
-    app.post('/todo',async(req,res)=>{
+    app.post("/todo", async (req, res) => {
       const service = req.body;
-      const result = await todoBooking.insertOne(service)
+      const result = await todoBooking.insertOne(service);
+      res.send(result);
+    });
+
+
+
+    app.get('/todo',async(req,res)=>{
+      const result = await todoBooking.find().toArray()
       res.send(result)
     })
+  } finally {
   }
-  finally{}
 }
 run().catch(console.dir);
 
-app.get('/', (req, res) => {
-  res.send('to-do-app')
-})
-
+app.get("/", (req, res) => {
+  res.send("to-do-app");
+});
 
 app.listen(port, () => {
-    console.log(`todo app ${port}`);
-  });
+  console.log(`todo app ${port}`);
+});
