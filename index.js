da59395ff34d19952eb7625060d5e06c8b23eb36
shariarhossain23e <shariarhossain23@gamil.comm>
2022-05-19 00:58:58 +0600

    initial setup

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const cors = require("cors")
+const app = express()
+require("dotenv").config()
+const port =process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+// middleware 
+app.use(cors())
+app.use(express.json())
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.foxet.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+app.get('/', (req, res) => {
+  res.send('to-do-app')
+})
+
+
+app.listen(port, () => {
+    console.log(`todo app ${port}`);
+  });
