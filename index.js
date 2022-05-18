e5af8cdd01451c3bc6c8d1d381ffc5c65332c9f0
shariarhossain23e <shariarhossain23@gamil.comm>
2022-05-19 01:16:12 +0600

    post api

--- index.js
+++ index.js
@@ -13,6 +13,21 @@ app.use(express.json())
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.foxet.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+async function run(){
+  try{
+    await client.connect();
+
+    const todoBooking = client.db("to_do").collection("data");
+
+    app.post('/todo',async(req,res)=>{
+      const service = req.body;
+      const result = await todoBooking.insertOne(service)
+      res.send(result)
+    })
+  }
+  finally{}
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
   res.send('to-do-app')
