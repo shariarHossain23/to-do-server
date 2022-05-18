2a81aa973be73cb9a62a60932504f0d750d4cfef
shariarhossain23e <shariarhossain23@gamil.comm>
2022-05-19 04:46:16 +0600

    end

--- index.js
+++ index.js
@@ -35,6 +35,7 @@ async function run() {
       res.send(result)
     })
     
+    
     app.delete('/todo/:id',async(req,res)=>{
       const id = req.params.id;
       const filterId = {_id:ObjectId(id)}
